d22cbc492c87aba949fd70eb125109593e3bb173
Matt <frozenpoint@gmail.com>
2015-11-02 16:03:44 -0500

    Removing unused cruft from references directory prior to public sharing

--- reference/demo/slave/main.h
+++
@@ -1,7 +0,0 @@
-#include        "global.h"
-/****************************************/
-    /*  declare variable */
-uchar rece_buff[cFIFOlength] = {0xaa};   
-/****************************************/
-     /* declare founction */
-     
